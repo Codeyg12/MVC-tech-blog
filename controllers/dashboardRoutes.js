785e9141708d9d6f84336f63c70886bd953c06c4
Codeyg12 <codey.gallup@gmail.com>
2022-08-30 17:35:30 -0600

    added dashboard route, started views

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const { Post } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const userPosts = await Post.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+
+    const posts = userPosts.map((post) => post.get({ plain: true }));
+    res.render("dashboardPosts", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.redirect("login");
+  }
+});
