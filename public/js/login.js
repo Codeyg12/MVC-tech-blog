7354556441794eef2b50fe629cc3b0c40222ea00
Codeyg12 <codey.gallup@gmail.com>
2022-09-04 10:53:55 -0600

    seperated login and signup

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,3 @@
-const signupHandler = async (e) => {
-  e.preventDefault();
-
-  const username = document.querySelector("#nameSign").value.trim();
-  const email = document.querySelector("#emailSign").value.trim();
-  const password = document.querySelector("#passSign").value.trim();
-
-  if (username && email && password) {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({ username, email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      document.location.replace("/dashboard");
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
 const loginHandler = async (e) => {
   e.preventDefault();
 
@@ -40,5 +18,5 @@ const loginHandler = async (e) => {
   }
 };
 
-document.querySelector(".signupForm").addEventListener("submit", signupHandler);
+
 document.querySelector(".loginForm").addEventListener("submit", loginHandler);
